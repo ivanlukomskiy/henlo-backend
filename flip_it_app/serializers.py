db561f7970b4201f7ec0aa4bf90ee1d6d20afee8
ivanl <ivanlukomskiy@gmail.com>
2020-02-04 04:22:11 +0300

    Drafts support

--- flip_it_app/serializers.py
+++ flip_it_app/serializers.py
@@ -5,12 +5,13 @@ from .models import Translation
 
 
 class TranslationSerializer(serializers.ModelSerializer):
-    original = serializers.CharField(required=False, default='')
-    translation = serializers.CharField(required=False, default='')
+    original = serializers.CharField(allow_blank=True, required=False, default='', max_length=500)
+    translation = serializers.CharField(allow_blank=True, required=False, default='', max_length=500)
     added = TimestampField()
     updated = TimestampField(required=False, default=None, allow_null=True)
     deleted = serializers.BooleanField(required=False, default=False)
     starred = serializers.BooleanField(required=False, default=False)
+
     class Meta:
         model = Translation
         fields = ['uuid', 'original', 'translation', 'added', 'updated', 'starred', 'deleted']
