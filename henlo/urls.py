2bf6316fe1fe1f40d4b142c11872ec9b6b31cf86
ivanlukomskiy <ivanlukomskiy@yandex-team.ru>
2022-05-03 00:27:19 +0300

    Add django extensions

--- henlo/urls.py
+++ henlo/urls.py
@@ -5,9 +5,11 @@ from rest_framework import routers
 from henlo_app import views
 
 router = routers.DefaultRouter()
+admin.site.site_header = 'Henlo administration'
+admin.site.index_title = ''
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/v1/sync', views.sync),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
